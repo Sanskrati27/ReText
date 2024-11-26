43d70a5e76bbd958f30008f5680ec2721c226d6e
Sanskrati Varshney <123263038+Sanskrati27@users.noreply.github.com>
2024-11-26 12:43:22 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { PropTypes } from "prop-types";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 export default function Navbar(props) {
     return (
         <>
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-            <Link className="navbar-brand" to="/">
+            <a className="navbar-brand" href="/">
                 {props.title}
-            </Link>
+            </a>
         
             <button
                 className="navbar-toggler"
@@ -24,14 +24,14 @@ export default function Navbar(props) {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item ">
-                        <Link className="nav-link" to="/">
+                        <a className="nav-link" href="/">
                             Home <span className="sr-only">(current)</span>
-                        </Link>
+                        </a>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/about">
-                            {props.about}
-                        </Link>
+                        // <Link className="nav-link" to="/about">
+                        //     {props.about}
+                        // </Link>
                     </li>
                 </ul>
                 {/* <form className="form-inline my-2 my-lg-0">
